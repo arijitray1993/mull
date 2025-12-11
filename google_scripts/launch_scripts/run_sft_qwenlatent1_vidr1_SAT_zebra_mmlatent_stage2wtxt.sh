553d5dd704831e7461fb963c2d067e7be5f9dee3
Arijit Ray <arijitray@google.com>
2025-12-11 23:59:45 +0000

    added hf accelerator launch commands

---
+++ google_scripts/launch_scripts/run_sft_qwenlatent1_vidr1_SAT_zebra_mmlatent_stage2wtxt.sh
@@ -0,0 +1,33 @@
+# needs to be run from root
+# bash google_scripts/launch_scripts/run_sft_qwenlatent1_vidr1_SAT_zebra_mmlatent_stage2wtxt.sh
+
+export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
+export LOG_PATH="./debug_log_2b.txt"
+export WANDB_MODE="offline"
+
+# set the exp config file
+EXP_CONFIG_FILE="../../google_scripts/exp_configs/vidr1_sat_zebra_sft_mmlatent2wtxt_qwenlatent1.yaml"
+
+cd src/r1-v && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 torchrun --nproc_per_node="6" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12349" \
+    src/open_r1/sft_video.py \
+    --exp_conf_file $EXP_CONFIG_FILE  \
+    --output_dir "" \
+    --model_name_or_path "" \
+    --dataset_name "" \
+    --deepspeed local_scripts/zero2.json \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --learning_rate 1e-6 \
+    --logging_steps 1 \
+    --bf16 \
+    --report_to wandb \
+    --gradient_checkpointing false \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 1 \
+    --run_name ""  \
+    --save_steps 4000 \
+    --max_grad_norm 5
---
+++ google_scripts/launch_scripts/run_sft_video.sh
@@ -0,0 +1,34 @@
+# needs to be run from src/r1-v 
+# bash ../scripts/run_sft_video.sh
+
+export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
+export LOG_PATH="./debug_log_2b.txt"
+export WANDB_MODE="offline"
+
+# set the exp config file
+EXP_CONFIG_FILE="../../configs/exp_configs/sat_test_sft.yaml"
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4 torchrun --nproc_per_node="5" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12349" \
+    src/open_r1/sft_video.py \
+    --exp_conf_file $EXP_CONFIG_FILE  \
+    --output_dir "" \
+    --model_name_or_path "" \
+    --dataset_name "" \
+    --deepspeed local_scripts/zero2.json \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --learning_rate 1e-6 \
+    --logging_steps 1 \
+    --bf16 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 1 \
+    --run_name ""  \
+    --save_steps 1000 \
+    --max_grad_norm 5 \
+    --save_only_model true
